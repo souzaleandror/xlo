f368878f171269fc35601f0a5ed66ebaa55e44b8
souza.leandror@gmail.com <souza.leandror@gmail.com>
2020-02-12 00:05:29 +0100

    Update 30%

---
+++ lib/models/filter.dart
@@ -0,0 +1,8 @@
+enum OrderBy { DATE, PRICE }
+
+// 0000
+
+const VENDOR_TYPE_PARTICULAR = 1 << 0; // 00001
+const VENDOR_TYPE_PROFESSIONAL = 1 << 1; // 0010
+
+// VENDOR_TYPE_PARTICULAR | VENDOR_TYPE_PROFESSIONAL 0011 (Ou logico BitFlag Rotacao de bit)
--- lib/screens/filter/filter_screen.dart
+++ lib/screens/filter/filter_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:xlo/models/filter.dart';
+import 'package:xlo/screens/filter/widgets/animated_button.dart';
+import 'package:xlo/screens/filter/widgets/order_by_field.dart';
+import 'package:xlo/screens/filter/widgets/price_rage_field.dart';
 import 'package:xlo/screens/filter/widgets/section_title.dart';
+import 'package:xlo/screens/filter/widgets/vendor_type_field.dart';
 
 class FilterScreen extends StatefulWidget {
   @override
@@ -8,6 +13,7 @@ class FilterScreen extends StatefulWidget {
 
 class _FilterScreenState extends State<FilterScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +27,26 @@ class _FilterScreenState extends State<FilterScreen> {
           Form(
             key: _formKey,
             child: ListView(
+              controller: _scrollController,
               padding: const EdgeInsets.all(16),
               children: <Widget>[
                 SectionTitle("Ordenar por"),
+                OrderByField(initalValue: OrderBy.DATE, onSaved: (v) {}),
                 SectionTitle("Preco RS"),
+                PriceRangeField(),
                 SectionTitle("Tipo de  anunciante"),
+                VendorTypeField(
+                    initalValue:
+                        VENDOR_TYPE_PROFESSIONAL | VENDOR_TYPE_PARTICULAR,
+                    onSaved: (s) {}),
               ],
             ),
           ),
+          AnimatedButton(
+              scrollController: _scrollController,
+              onTap: () {
+                print("teste");
+              }),
         ],
       ),
     );
